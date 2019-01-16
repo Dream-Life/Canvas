df46b9532413c1074df56a67a8eb3b794de68f22
dream-Y <1308082973@qq.com>
2019-01-16 18:01:42 +0800

    init

--- shopping/apps/goods/models.py
+++ shopping/apps/goods/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class GoodsCategory(models.Model):
+    """
+    商品类别
+    """
+    name = models.CharField()
+    code = models.CharField()
+    desc = models.CharField()
+    categroy_type = models.CharField()
